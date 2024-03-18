8c6b8f45bc1f5d8e347b36866094a36abd6630e1
Matt Christiansen <mattchristiansen59@gmail.com>
2024-03-18 15:51:50 -0700

    FEATURE: Set up routing

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,19 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Text('Home')),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            ElevatedButton.icon(
+              onPressed: () {
+                Navigator.pushNamed(context, '/choose_location');
+              },
+              icon: Icon(Icons.edit_location),
+              label: Text('Edit Location'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
